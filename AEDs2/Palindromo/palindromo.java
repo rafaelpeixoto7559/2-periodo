332980a5dc70ea529969995aa49b54f6715586b4
Rafael Oliveira Lima Peixoto <rafaelolpeixoto@gmail.com>
2023-08-03 17:43:26 -0300

    Organização utilizando método

--- AEDs2/Palindromo/palindromo.java
+++ AEDs2/Palindromo/palindromo.java
@@ -15,34 +15,45 @@ import java.util.Scanner;
     A entrada termina com a leitura de FIM
  */
 
-public class palindromo{
-    public static void main(String[] args){
+public class palindromo {
+
+    public static boolean verifica(String x) {
+
+        int tam = x.length();
+        boolean vale = true;
+
+        for (int i = 0; i < tam; i++) {
+            if (x.charAt(i) != x.charAt(tam - i - 1)) {
+                vale = false;
+            }
+        }
+
+        if (vale == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int res = 1;
-
-        do{
+        do {
             String x = sc.nextLine();
-            if(x.compareTo("FIM")==0){
+            boolean vale;
+
+            if (x.compareTo("FIM") == 0) {
                 res = 0;
-            }else{
-                int tam = x.length(), vale = 1;
-
-                for(int i = 0; i < tam; i++){
-                    if(x.charAt(i) != x.charAt(tam-i-1)){
-                        vale = 0;
-                    }
-                }
-
-                if(vale == 1){
+            } else {
+                vale = verifica(x);
+                if (vale == true) {
                     System.out.println("SIM");
-                }else{
+                } else {
                     System.out.println("NAO");
                 }
             }
-
-        }while(res == 1);
-
+        } while (res == 1);
         sc.close();
     }
 }
