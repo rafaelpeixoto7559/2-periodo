900e94c0038fdd1970fff14c9c8ef6845be34f3b
Rafael <rafaelolpeixoto@gmail.com>
2023-09-01 15:42:30 -0300

    Alteração aleatória

--- AEDs2/Trabalho-Pratico-01/Alteracao-aleatoria/Aleatorio.java
+++ AEDs2/Trabalho-Pratico-01/Alteracao-aleatoria/Aleatorio.java
@@ -1,5 +1,17 @@
 import java.util.Random;
 
+/*
+ * Crie um método iterativo que recebe uma string, sorteia duas letras minúsculas aleatórias (código ASCII >= 'a' e <= 'z'), substitui todas as ocorrências da primeira letra na string pela segunda e retorna a string com as alterações efetuadas. Na saída padrão, para cada linha de entrada, execute o método desenvolvido nesta questão e mostre a string retornada como uma linha de saída. Abaixo, observamos um exemplo de entrada supondo que para a primeira linha as letras sorteados foram o 'a' e o 'q'. Para a segunda linha, foram o 'e' e o 'k'.
+
+
+EXEMPLO DE ENTRADA:                 EXEMPLO DE SAÍDA:
+
+    
+o rato roeu a roupa do rei de roma       o rqto roeu q roupq do rei de romq
+    
+e qwe qwe qwe ewq ewq ewq            k qwk qwk qwk kwq kwq kwq
+ */
+
 public class Aleatorio {
     
     public static String aleatorio(String pal, char n1, char n2){
